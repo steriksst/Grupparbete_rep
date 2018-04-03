8b5bff492f470b55a228b1099e72befb2c64fd91
safr1701 <36695181+safr1701@users.noreply.github.com>
2018-04-03 11:15:36 +0200

    Uppd

--- Grupparbete/Grupparbete/Form1.cs
+++ Grupparbete/Grupparbete/Form1.cs
@@ -30,18 +30,8 @@ namespace Grupparbete
             SqlCommand MyQuery = new SqlCommand("select * from accepted_2007_to_2017", connect);
 
             SqlDataReader MyReader = MyQuery.ExecuteReader();
-            
-            chart1.Titles.Add("Loan Purpose");
-            chart1.ChartAreas[0].AxisX.Title = "";
-            chart1.ChartAreas[0].AxisY.Title = "";
-            chart1.Series["Series1"].ChartType = SeriesChartType.Column;
 
-            chart2.Titles.Add("Average int_rate / loan purpose");
-            chart2.ChartAreas[0].AxisX.Title = "";
-            chart2.ChartAreas[0].AxisY.Title = "";
-            chart2.Series["Series1"].ChartType = SeriesChartType.Point;
-
-            List<Loans> Loans = new List<Loans>();
+            List<Loans> LoanList = new List<Loans>();
 
             double loan_amount;
             double int_rate;
@@ -57,14 +47,30 @@ namespace Grupparbete
                 loan_purpose = MyReader["loan_purpose"].ToString();
                 total_rec_int = Convert.ToDouble(MyReader["total_rec_int"]);
 
-                Loans.Add(new Loans(
+                LoanList.Add(new Loans(
                     loan_amount,
                     int_rate,
                     grade,
                     loan_purpose,
                     total_rec_int
                     ));
-            }     
+            }
+
+            chart1.Titles.Add("Loan Purpose");
+            chart1.ChartAreas[0].AxisX.Title = "";
+            chart1.ChartAreas[0].AxisY.Title = "";
+            chart1.Series["Series1"].ChartType = SeriesChartType.Column;
+
+            chart2.Titles.Add("Average int_rate / loan purpose");
+            chart2.ChartAreas[0].AxisX.Title = "";
+            chart2.ChartAreas[0].AxisY.Title = "";
+            chart2.Series["Series1"].ChartType = SeriesChartType.Point; 
+
+            foreach (Loans L in LoanList.Where(x => x.Loan_purpose == "Other"))
+            {
+                chart1.Series["Series1"].Points.AddY(L.Loan_purpose);
+            }
+
         }
     }
 }
